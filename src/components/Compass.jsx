451f233b999cb10608205982921dbbb285a1ca58
Haim Faraj <haimfaraj@gmail.com>
2023-08-21 22:59:15 +0300

    fix

--- src/components/Compass.jsx
+++ src/components/Compass.jsx
@@ -5,7 +5,7 @@ export default class Compass extends React.Component {
 render() {
     
     const { min = 0, max = 360, value = 0 } = this.props;
-    var angle = (value - min) / (max - min) * 360;
+    var angle = (value - min) / (max - min) * 360; //no need for calculation  - 90 and inv 90 and 180?
     return (<div class="artificial-horizon">
         <div class="label label-0">0</div>
         <div class="label label-90">90</div>
